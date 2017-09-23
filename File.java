a0ddbd14caf71963adc913af90e25bbe041d08ac
Gulshan Raj <gulshan@cambridgecloudpartners.com>
2017-09-23 16:56:55 +0530

    First Change

--- File.java
+++ File.java
@@ -1,10 +1,9 @@
 public class abc
 {
 
-  // checking from windows branch
-  // checking from mac branch actually
-
-  // checking from mac branch actually but changed again
-  // one more line added
+  public void main()
+  {
+    
+  }
 
 }
